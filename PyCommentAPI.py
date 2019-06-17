5025b523844f89fa6a2b5d0fe72ddcd915d8c36a
Feno4ka <fenafenol@gmail.com>
2019-06-17 23:01:10 +0300

    second update

--- PyCommentAPI.py
+++ PyCommentAPI.py
@@ -1,13 +1,15 @@
 import apihelper
 
-
 class Comments:
-    def __init__(self, token):
+    def __init__(self, token, owner=None):
         self.token = token
+        self.owner = owner
 
-    def create_post(self, owner_id, type, text=None, photo_url=None,
+    def create_post(self, type, owner_id=None, text=None, photo_url=None,
             caption=None, parse_mode=None, administrators=None, disable_notifications=None):
         method_url = r'createPost'
+        if not owner_id:
+            owner_id = self.owner
         payload = {'owner_id': str(owner_id), 'type': type}
         if text:
             payload['text'] = text
@@ -22,6 +24,7 @@ class Comments:
         if disable_notifications:
             payload['disable_notifications'] = disable_notifications
         return apihelper._make_request(self.token, method_url, payload)
+
     def edit_post(self, post_id, text=None, photo_url=None, caption=None, parse_mode=None):
         method_url = r'editPost'
         payload = {'post_id': post_id}
--- apihelper.py
+++ apihelper.py
@@ -24,15 +24,37 @@ def _make_request(token, method_name, params=None):
         raise CommentsApiException(msg, method_name, result)
 
     if method_name == 'createPost':
-        return request(result_json['result']['post_id'], result_json['result']['link'])
+        return Comments(result_json['result']['post_id'], result_json['result']['link'], token)
 
-    elif method_name in ['editPost', 'deletePost']:
-        return request(params['post_id'], f"https://comments.bot/thread/{params['post_id']}")
+    elif method_name == 'editPost':
+        return Comments(params['post_id'], f"https://comments.bot/thread/{params['post_id']}", token)
 
-class request:
-    def __init__(self, post_id, link):
+    elif method_name == 'deletePost':
+        return result_json['ok']
+        
+class Comments:
+    def __init__(self, post_id, link, token):
         self.id = post_id
         self.link = link
+        self.token = token
+
+    def edit_post(self, text=None, photo_url=None, caption=None, parse_mode=None):
+        method_url = r'editPost'
+        payload = {'post_id': self.id}
+        if text:
+            payload['text'] = text
+        if photo_url:
+            payload['photo_url'] = photo_url
+        if caption:
+            payload['caption'] = caption
+        if parse_mode:
+            payload['parse_mode'] = parse_mode
+        return _make_request(self.token, method_url, payload)
+
+    def delete_post(self):
+        method_url = r'deletePost'
+        payload = {'post_id': self.id}
+        return _make_request(self.token, method_url, payload)
 
 class CommentsApiException(Exception):
     def __init__(self, msg, function_name, result):
